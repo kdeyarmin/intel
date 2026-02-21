2fbee2f7d132f92da2499a7a452f0c8cb0ab571a
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-02-21 05:38:41 +0000

    File changes

--- functions/findSNFUrl.ts
+++ functions/findSNFUrl.ts
@@ -2,9 +2,6 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import * as cheerio from 'npm:cheerio@1.0.0-rc.12';
 
 Deno.serve(async (req) => {
-  const base44 = createClientFromRequest(req);
-  const user = await base44.auth.me();
-  
   const pageUrl = 'https://data.cms.gov/summary-statistics-on-use-and-payments/medicare-medicaid-service-type-reports/cms-program-statistics-medicare-skilled-nursing-facility';
   
   try {
@@ -15,21 +12,28 @@ Deno.serve(async (req) => {
     const links = [];
     $('a').each((i, el) => {
       const href = $(el).attr('href');
-      if (href && (href.endsWith('.zip') || href.includes('files/'))) {
+      // Log ANY href that looks like a file or external link
+      if (href && (href.includes('.zip') || href.includes('files/') || href.includes('download') || href.includes('MDCR'))) {
         links.push(href);
       }
     });
 
-    // Also look for buttons/elements that might hold the download link
-    const potential = [];
+    const buttons = [];
     $('button').each((i, el) => {
-       const txt = $(el).text();
-       if (txt.includes('Download')) {
-           potential.push($(el).html());
-       }
+        // sometimes the link is in a data attribute
+        buttons.push($(el).attr('data-url') || $(el).text());
+    });
+    
+    // Look for script tags that might contain the download link
+    const scripts = [];
+    $('script').each((i, el) => {
+        const txt = $(el).html();
+        if (txt && txt.includes('.zip')) {
+            scripts.push(txt.substring(0, 500));
+        }
     });
 
-    return Response.json({ links, potential });
+    return Response.json({ links, buttons, scripts });
   } catch (error) {
     return Response.json({ error: error.message });
   }
