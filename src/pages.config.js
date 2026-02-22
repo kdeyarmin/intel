62813325b5269c013d72f18d7cf426c3bff774b0
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-02-22 21:35:09 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -48,36 +48,26 @@
  * The mainPage value must match a key in the PAGES object exactly.
  */
 import AIAssistant from './pages/AIAssistant';
-import AIInsights from './pages/AIInsights';
 import AdvancedAnalytics from './pages/AdvancedAnalytics';
-import Analytics from './pages/Analytics';
 import AuditLog from './pages/AuditLog';
-import AutoImports from './pages/AutoImports';
-import BulkEmailExport from './pages/BulkEmailExport';
 import CMSAnalytics from './pages/CMSAnalytics';
 import CustomReports from './pages/CustomReports';
 import Dashboard from './pages/Dashboard';
 import DataCenter from './pages/DataCenter';
-import DataImports from './pages/DataImports';
 import DataQuality from './pages/DataQuality';
 import EmailSearchBot from './pages/EmailSearchBot';
 import EnrichmentHub from './pages/EnrichmentHub';
-import ErrorReports from './pages/ErrorReports';
 import Help from './pages/Help';
 import ImportAnalytics from './pages/ImportAnalytics';
-import ImportHub from './pages/ImportHub';
 import ImportMonitoring from './pages/ImportMonitoring';
-import ImportSchedule from './pages/ImportSchedule';
 import LeadListBuilder from './pages/LeadListBuilder';
 import LeadLists from './pages/LeadLists';
-import LocationAnalytics from './pages/LocationAnalytics';
 import LocationDetail from './pages/LocationDetail';
 import Locations from './pages/Locations';
 import MAInpatientDashboard from './pages/MAInpatientDashboard';
 import NPPESCrawler from './pages/NPPESCrawler';
 import NPPESCrawlerDashboard from './pages/NPPESCrawlerDashboard';
 import NPPESCrawlerSettings from './pages/NPPESCrawlerSettings';
-import NPPESImport from './pages/NPPESImport';
 import OrganizationDetail from './pages/OrganizationDetail';
 import Organizations from './pages/Organizations';
 import ProviderDetail from './pages/ProviderDetail';
@@ -86,45 +76,33 @@ import ProviderOutreach from './pages/ProviderOutreach';
 import Providers from './pages/Providers';
 import ReferralNetworkIntelligence from './pages/ReferralNetworkIntelligence';
 import ReferralPathwayAnalysis from './pages/ReferralPathwayAnalysis';
-import Referrals from './pages/Referrals';
 import ScoringRules from './pages/ScoringRules';
 import TerritoryIntelligence from './pages/TerritoryIntelligence';
-import Utilization from './pages/Utilization';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "AIAssistant": AIAssistant,
-    "AIInsights": AIInsights,
     "AdvancedAnalytics": AdvancedAnalytics,
-    "Analytics": Analytics,
     "AuditLog": AuditLog,
-    "AutoImports": AutoImports,
-    "BulkEmailExport": BulkEmailExport,
     "CMSAnalytics": CMSAnalytics,
     "CustomReports": CustomReports,
     "Dashboard": Dashboard,
     "DataCenter": DataCenter,
-    "DataImports": DataImports,
     "DataQuality": DataQuality,
     "EmailSearchBot": EmailSearchBot,
     "EnrichmentHub": EnrichmentHub,
-    "ErrorReports": ErrorReports,
     "Help": Help,
     "ImportAnalytics": ImportAnalytics,
-    "ImportHub": ImportHub,
     "ImportMonitoring": ImportMonitoring,
-    "ImportSchedule": ImportSchedule,
     "LeadListBuilder": LeadListBuilder,
     "LeadLists": LeadLists,
-    "LocationAnalytics": LocationAnalytics,
     "LocationDetail": LocationDetail,
     "Locations": Locations,
     "MAInpatientDashboard": MAInpatientDashboard,
     "NPPESCrawler": NPPESCrawler,
     "NPPESCrawlerDashboard": NPPESCrawlerDashboard,
     "NPPESCrawlerSettings": NPPESCrawlerSettings,
-    "NPPESImport": NPPESImport,
     "OrganizationDetail": OrganizationDetail,
     "Organizations": Organizations,
     "ProviderDetail": ProviderDetail,
@@ -133,10 +111,8 @@ export const PAGES = {
     "Providers": Providers,
     "ReferralNetworkIntelligence": ReferralNetworkIntelligence,
     "ReferralPathwayAnalysis": ReferralPathwayAnalysis,
-    "Referrals": Referrals,
     "ScoringRules": ScoringRules,
     "TerritoryIntelligence": TerritoryIntelligence,
-    "Utilization": Utilization,
 }
 
 export const pagesConfig = {
